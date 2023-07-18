3b8e00ee74abf4875a9aac75819b2666ee5cb0aa
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-07-18 16:25:35 +0300

    .

--- crypto-art/src/App.jsx
+++ crypto-art/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 				<NavBar />
 			</header>
 			<main>
+				{/* 
 				<HeroSection />
 				<BrandsBanner />
 				<AboutSection />
@@ -24,9 +25,10 @@ function App() {
 				<Divider />
 				<FaqSection />
 				<JoinSection />
-				<Divider />
+				<Divider /> 
+				*/}
 			</main>
-			<footer>
+			{/* <footer>
 				<Footer />
 				<div className={classes.wrapper}>
 					<a
@@ -36,7 +38,7 @@ function App() {
 						DESIGN USED
 					</a>
 				</div>
-			</footer>
+			</footer> */}
 		</>
 	);
 }
